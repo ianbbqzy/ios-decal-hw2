3f292346130d84794509bb987bc04db0064911a9
Ian Lee <ianlee@airbears2-10-142-49-28.airbears2.1918.berkeley.edu>
2016-02-16 22:41:42 -0800

    done

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -47,6 +47,45 @@ class KeyboardViewController: UIInputViewController {
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
         
     }
+    
+    @IBAction func returnButton(sender: UIButton) {
+        textDocumentProxy.insertText("\n")
+    }
+    
+    @IBAction func deleteButton(sender: UIButton) {
+        textDocumentProxy.deleteBackward()
+    }
+    
+    @IBAction func test3Button(sender: UIButton) {
+        textDocumentProxy.insertText("(-‿‿-)")
+    }
 
-
+    @IBAction func test1Button(sender: UIButton) {
+        textDocumentProxy.insertText("(´∀｀*)")
+    }
+    
+    @IBAction func test2Button(sender: UIButton) {
+        textDocumentProxy.insertText("(*^ω^)")
+    }
+    
+    @IBAction func test4Button(sender: UIButton) {
+        textDocumentProxy.insertText("☆*:.｡.o(≧▽≦)o.｡.:*☆")
+    }
+    
+    @IBAction func test5Button(sender: UIButton) {
+        textDocumentProxy.insertText("(*≧ω≦*)")
+    }
+    
+    @IBAction func test6Button(sender: UIButton) {
+        var c = String(UnicodeScalar(0x1F64F))
+        for i in 0x1F601...0x1F64F {
+            c = String(UnicodeScalar(i))
+            let diceRoll = Int(arc4random_uniform(6) + 1)
+            if (diceRoll % 2 == 0){
+                textDocumentProxy.insertText(c)
+                return
+            }
+        }
+        textDocumentProxy.insertText(c)
+    }
 }
